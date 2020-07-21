ab3daf9de75b9aabaf7b91698fba568d70b042cc
ingsergiovt <ingsergiovt@gmail.com>
2020-07-21 19:03:30 -0400

    routes

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'QuoteController@create');
+Route::get('/form/{code?}', 'QuoteController@create')->name('get.form');
+Route::post('/form/profile', 'QuoteController@store_profile')->name('form.profile.store');
+Route::post('/form/vehicle', 'QuoteController@store_vehicle')->name('form.vehicle.store');
+Route::post('/form/driver', 'QuoteController@store_driver')->name('form.driver.store');
+Route::post('/form/coverage', 'QuoteController@store_quote')->name('form.coverage.store');
+
+Route::get('/tanks', function(){
+    return view('quote.thanks');
+})->name('thanks');
+
 
 Auth::routes();
 
